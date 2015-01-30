74c8ac8737bfbc52d6bc6844f53dad53a9e9af21
Alexander Wong <admin@alexander-wong.com>
2015-01-30 16:49:57 -0700

    Added index with semantic ui demo

--- apollo/urls.py
+++ apollo/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from apollo.views import index
+from apollo.views import index, ws_demo
 
 admin.autodiscover()
 urlpatterns = patterns(
     '',
     url(r'^$', index, name='index'),
+    url(r'^ws_demo/', ws_demo, name='ws_demo'),
     url(r'^admin/', include(admin.site.urls)),
 )
--- apollo/views.py
+++ apollo/views.py
@@ -3,4 +3,8 @@ from django.template import RequestContext
 
 
 def index(request):
+    return render_to_response('index.html', {}, context_instance=RequestContext(request))
+
+
+def ws_demo(request):
     return render_to_response('demo.html', {}, context_instance=RequestContext(request))
